7425b53be4964e6159feac9a1fbdd9d4ee0d66e7
Yurly123 <joo7won@gmail.com>
2024-06-21 23:23:14 +0900

    Add Hello box
    It hellos to you!

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,53 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import '@styles/app.scss';
-import icons from '@components/icons';
 
 const Application: React.FC = () => {
-  const [counter, setCounter] = useState(0);
-  const [darkTheme, setDarkTheme] = useState(true);
-  const [versions, setVersions] = useState<Record<string, string>>({});
-
-  /**
-   * On component mount
-   */
-  useEffect(() => {
-    const useDarkTheme = parseInt(localStorage.getItem('dark-mode'));
-    if (isNaN(useDarkTheme)) {
-      setDarkTheme(true);
-    } else if (useDarkTheme == 1) {
-      setDarkTheme(true);
-    } else if (useDarkTheme == 0) {
-      setDarkTheme(false);
-    }
-
-    // Apply verisons
-    const app = document.getElementById('app');
-    const versions = JSON.parse(app.getAttribute('data-versions'));
-    setVersions(versions);
-  }, []);
-
-  /**
-   * On Dark theme change
-   */
-  useEffect(() => {
-    if (darkTheme) {
-      localStorage.setItem('dark-mode', '1');
-      document.body.classList.add('dark-mode');
-    } else {
-      localStorage.setItem('dark-mode', '0');
-      document.body.classList.remove('dark-mode');
-    }
-  }, [darkTheme]);
-
-  /**
-   * Toggle Theme
-   */
-  function toggleTheme() {
-    setDarkTheme(!darkTheme);
-  }
-
   return (
-    <div className='hello'>Hello</div>
+    <div className='hello'>
+      <div className='hello-box'>
+        Hello
+      </div>
+    </div>
   );
 };
 
