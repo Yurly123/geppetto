bbf650acab3b7c2b8821f06e3a04718b586d17a5
Yurly123 <joo7won@gmail.com>
2024-06-23 00:38:09 +0900

    Complete subtitle component
    It was damn hard y'know?

--- src/renderer/components/Chat/Subtitle.tsx
+++ src/renderer/components/Chat/Subtitle.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { CSSProperties, useEffect } from 'react';
 import '@styles/chat.scss';
 
 type Props = {
   message?: string;
+  isVisible?: boolean;
+  disappearDelay?: number;
 }
 
 const Subtitle: React.FC<Props> = (props) => {
-  return (
-    <div className='chat-subtitle'>
+  const [style, setStyle] = React.useState<CSSProperties>({});
+  const disappear: CSSProperties = {
+    animation: `${props.disappearDelay || 3}s disappear`,
+    opacity: 0,
+  }
+
+  useEffect(() => {
+    if (props.isVisible) setStyle({});
+    else setStyle(disappear);
+  }, [props.isVisible]);
+
+  return props.message ? (
+    <div className='chat-subtitle' style={style}>
       <div className='chat-subtitle-box'>
         <span>
           {props.message}
         </span>
       </div>
     </div>
-  );
+  ) : null;
 };
 
 export default Subtitle;
--- src/renderer/components/Chat/index.tsx
+++ src/renderer/components/Chat/index.tsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '@styles/chat.scss';
 import ChatBar from './ChatBar';
 import Subtitle from './Subtitle';
 
 const Chat: React.FC = () => {
   const [message, setMessage] = React.useState<string>('');
+  const [messages, setMessages] = React.useState<string[]>([]); 
+  const [subtitleVisible, setSubtitleVisible] = React.useState<boolean>(false);
+
+  useEffect(() => {
+    if (!subtitleVisible) return;
+    const length = messages[messages.length - 1]?.length || 0;
+    const timer = setTimeout(() => {
+      setSubtitleVisible(false);
+    }, 2000 + length * 100);
+    return () => clearTimeout(timer);
+  }, [subtitleVisible]);
 
   return (
     <div className='chat'>
-      <Subtitle message={message} />
+      <Subtitle 
+        message={messages[messages.length - 1]} 
+        isVisible={subtitleVisible}
+      />
       <div style={{ height: '100%' }}></div>
       <ChatBar 
         message={message}
         onMessageChange={setMessage}
         onMessageSubmit={(message) => {
           console.log('Submit:', message);
+          setMessages([...messages, message]);
           setMessage('');
+          setSubtitleVisible(true);
         }}
       />
     </div>
