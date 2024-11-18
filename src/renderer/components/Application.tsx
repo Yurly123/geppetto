f87525fcddcb37edc8be648b6c0b07ff71fa8834
Yurly123 <joo7won@gmail.com>
2024-11-18 11:50:34 +0900

    Add menu button asdf

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import '@styles/app.scss';
 import { ContextsProvider } from './contexts';
 import Chat from './Chat';
-import Viseme from './Viseme';
+import MenuButton from './Menu/MenuButton';
 
 const Application: React.FC = () => {
   return (
     <ContextsProvider>
       <div className='app'>
-        <div className='box'>
-          <Viseme />
-        </div>
+        <MenuButton />
         <Chat />
       </div>
     </ContextsProvider>
---
+++ src/renderer/components/Menu/MenuButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '@styles/menu.scss';
+
+const MenuButton: React.FC = () => {
+    return (
+        <div className='menu-button'>
+            <h3 onClick={null}>메뉴</h3>
+        </div>
+    )
+}
+
+export default MenuButton;
