c8ded3636879d1f6763167bd79ee81ef8db55659
Yurly123 <joo7won@gmail.com>
2024-11-22 12:04:23 +0900

    Make initializing log
    But I'm afraid I'm gonna keep it later

--- src/renderer/components/Menu/Log/InitializeButton.tsx
+++ src/renderer/components/Menu/Log/InitializeButton.tsx
@@ -1,8 +1,18 @@
+import { systemMessage } from '@common/openai';
+import { DispatchMessagesContext } from '@components/contexts';
 import { Modal } from '@components/util';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 
 const InitializeButton: React.FC = () => {
     const [modalEnable, setModalEnable] = useState(false);
+    const dispatchMessages = useContext(DispatchMessagesContext);
+
+    function initializeLog() {
+        window.store.saveMessages([systemMessage()]);
+        window.store.loadMessages().then((messages) => {
+            dispatchMessages({ type: 'changeAll', messages });
+        });
+    }
 
     return <>
         <h3
@@ -14,8 +24,16 @@ const InitializeButton: React.FC = () => {
         <Modal
             enable={modalEnable}
             setEnable={setModalEnable}
+            onClose={(result) => {
+                result && initializeLog();
+            }}
         >
-            <h1>모달</h1>
+            <h3>로그 초기화</h3>
+            <p style={{ margin: '0', fontSize: '1.75rem' }}>
+                로그를 초기화하시겠습니까? <br />
+                캐릭터와의 대화내용이 전부 삭제됩니다. <br />
+                그로인해 <span className='warning-color'>처음부터 다시 대화를 시작하게 됩니다.</span>
+            </p>
         </Modal>
     </>
 };
