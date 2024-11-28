a3397c0d50e1afae336c0bb104268749c4267765
Yurly123 <joo7won@gmail.com>
2024-11-28 10:13:59 +0900

    Fix chatbar styles
    I HATE CSS

--- src/renderer/components/Chat/ChatBar/ChatInput.tsx
+++ src/renderer/components/Chat/ChatBar/ChatInput.tsx
@@ -1,4 +1,3 @@
-import { HelpTrigger } from '@components/util';
 import React from 'react';
 
 type Props = {
@@ -8,15 +7,13 @@ type Props = {
 }
 
 const ChatInput: React.FC<Props> = (props) => {
-    return <HelpTrigger message='메세지를 입력하고 Enter를 누르면 전송됩니다. 전송하고 수 초 기다리면 응답이 옵니다.'>
-        <input
+    return <input
         disabled={!props.enable}
         type='text'
         placeholder='메세지 입력'
         value={props.message}
         onChange={(e) => props.onChange(e.target.value)}
     />
-    </HelpTrigger>
 };
 
 export default ChatInput;
--- src/renderer/components/Chat/ChatBar/index.tsx
+++ src/renderer/components/Chat/ChatBar/index.tsx
@@ -2,6 +2,7 @@ import React, { FormEvent, useContext, useState } from 'react';
 import ChatInput from './ChatInput';
 import { createMessage } from '@common/openai';
 import { DispatchMessagesContext } from '@components/contexts';
+import { HelpTrigger } from '@components/util';
 
 type Props = {
     enable?: boolean;
@@ -17,17 +18,18 @@ const ChatBar: React.FC<Props> = ({ enable }) => {
         const newMessage = createMessage('user', input);
         dispatchMessages({ type: 'add', message: newMessage });
     }
-
-    return <form
-        className='chat-bar'
-        onSubmit={handleSumbit}
-    >
-        <ChatInput
-            enable={enable}
-            message={input}
-            onChange={setInput}
-        />
-    </form>
+    return <HelpTrigger message='메세지를 입력하고 Enter를 누르면 전송됩니다. 전송하고 수 초 기다리면 응답이 옵니다.'>
+        <form
+            className='chat-bar'
+            onSubmit={handleSumbit}
+        >
+            <ChatInput
+                enable={enable}
+                message={input}
+                onChange={setInput}
+            />
+        </form>
+    </HelpTrigger>
 };
 
 export default ChatBar;
