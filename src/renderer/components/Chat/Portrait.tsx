cbb809edf3e035a2ba88b954892af7a33c2a08eb
Yurly123 <joo7won@gmail.com>
2024-12-02 12:21:31 +0900

    Import images
    NAI IS GOD

--- src/renderer/components/Chat/Portrait.tsx
+++ src/renderer/components/Chat/Portrait.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import portrait from '@assets/images/gepptto/neutral.png'
 
 const Portrait: React.FC = () => {
     return (
         <div className='portrait'>
-            <img src='https://placehold.co/512x1280' alt='portrait' />
+            <img src={portrait} alt='portrait' />
         </div>
     );
 }
