9ec4f7da45c060fa910ea42dfbc1b9816f970d2e
Yurly123 <joo7won@gmail.com>
2024-12-09 10:58:36 +0900

    Styled prompt modal input
    Input change modal cssscscscsdjcdslkfdj

--- src/renderer/components/Chat/ResponseBox/Controllers/InputChange.tsx
+++ src/renderer/components/Chat/ResponseBox/Controllers/InputChange.tsx
@@ -1,20 +1,34 @@
-import { HelpTrigger } from "@components/util";
-import React from "react";
+import { HelpTrigger, Modal, ModalRef } from "@components/util";
+import React, { useRef } from "react";
 
 const InputChange: React.FC = () => {
+    const modalRef = useRef<ModalRef>(null);
 
     function handleClick() {
+        modalRef.current?.open();
     }
-    return (
+    return <>
         <div
             className='input-change'
             onClick={handleClick}
         >
-            <HelpTrigger message='입력한 내용을 바꿀 수 있습니다. 현재의 응답은 저장되지 않고 새로운 응답으로 대체됩니다.'>
+            <HelpTrigger message='사용자가 현재 응답에서 입력했던 내용을 바꿀 수 있습니다. 현재의 응답은 저장되지 않고 새로운 응답으로 대체됩니다.'>
                 입력 바꾸기
             </HelpTrigger>
         </div>
-    );
+        <Modal
+            type='prompt'
+            ref={modalRef}
+            onClose={(result) => {
+                console.log(result);
+            }}
+        >
+            <h3>입력 바꾸기</h3>
+            <span>
+                새로운 입력을 입력해주세요.
+            </span>
+        </Modal>
+    </>
 }
 
 export default InputChange;
