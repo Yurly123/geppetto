57af72151a8035ad8b01f4d0664a829502e9cbd9
Yurly123 <joo7won@gmail.com>
2024-12-09 10:47:20 +0900

    CSS changes
    I hate CSS

--- src/renderer/components/Menu/Log/InitializeButton.tsx
+++ src/renderer/components/Menu/Log/InitializeButton.tsx
@@ -30,7 +30,7 @@ const InitializeButton: React.FC = () => {
             }}
         >
             <h3>로그 초기화</h3>
-            <p style={{ margin: '0', fontSize: '1.75rem' }}>
+            <p>
                 로그를 초기화하시겠습니까? <br />
                 캐릭터와의 대화내용이 전부 삭제됩니다. <br />
                 그로인해 <span className='warning-color'>처음부터 다시 대화를 시작하게 됩니다.</span>
